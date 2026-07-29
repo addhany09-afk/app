cf9cc994ccd1f448e68ca8f6f0b012f55be82b72
addhany09-afk <addhany09@gmail.com>
2026-07-29 14:36:12 +0300

    Create main.tsx

---
+++ bcare_project_files/src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Step1Vehicle from './pages/Step1Vehicle';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Step1Vehicle />
+  </React.StrictMode>
+);
